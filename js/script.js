aea94f97edde502b651c8f254e3a5e2ce759a418
bing6780 <hoainamyahooaz@gmail.com>
2026-07-01 12:47:50 +0700

    Add profile and avatar styles to script.js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,16 @@ function showMessage(){
     alert("Mời bạn sang trang Sản phẩm để xem toàn bộ điện thoại.");
 
 }
+.profile {
+    text-align: center;
+    margin: 40px 0;
+}
+
+.avatar {
+    width: 180px;
+    height: 180px;
+    border-radius: 50%;
+    object-fit: cover;
+    border: 4px solid #0066cc;
+    margin: 20px 0;
+}
